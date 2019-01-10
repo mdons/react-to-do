ec116a902757a5655758ed2ff436856f6110ef68
mdons <michaeljdonnell@gmail.com>
2019-01-09 22:12:29 -0600

    working version following kirupa tutorial

--- src/ToDoItems.js
+++ src/ToDoItems.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
+import FlipMove from "react-flip-move";
 
-class ToDoItems extends Component {
-  creatTasks(item) {
-    return <li key={item.key}>{item.text}</li>;
+class TodoItems extends Component {
+  constructor(props) {
+    super(props);
+    this.createTasks = this.createTasks.bind(this);
   }
+  delete(key) {
+    this.props.delete(key);
+  }
+  createTasks(item) {
+    return (
+      <li onClick={() => this.delete(item.key)} key={item.key}>
+        {item.text}
+      </li>
+    );
+  }
+
   render() {
-    const toDoEntries = this.props.entries;
-    const listItems = toDoEntries.map(this.createTasks);
-    return <ul className="theList">{listItems}</ul>;
+    const todoEntries = this.props.entries;
+    const listItems = todoEntries.map(this.createTasks);
+    return (
+      <ul className="theList">
+        <FlipMove duration={250} easing="ease-out">
+          {listItems}
+        </FlipMove>
+      </ul>
+    );
   }
 }
 
-export default ToDoItems;
+export default TodoItems;
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,26 +1,59 @@
 import React, { Component } from "react";
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
 
-class ToDoList extends Component {
-  componentDidUpdate() {
-    this.props.inputElement.current.focus();
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+  addItem(e) {
+    if (this._inputElement.value !== "") {
+      const newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+
+      this.setState(prevState => {
+        return {
+          items: prevState.items.concat(newItem)
+        };
+      });
+
+      this._inputElement.value = "";
+    }
+    console.log(this.state.items);
+    e.preventDefault();
+  }
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter(function(item) {
+      return item.key !== key;
+    });
+
+    this.setState({
+      items: filteredItems
+    });
   }
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
-          <form onSubmit={this.props.addItem}>
+          <form onSubmit={this.addItem}>
             <input
-              placeholder="Task"
-              ref={this.props.inputElement}
-              value={this.props.currentItem.text}
-              onChange={this.props.handleInput}
+              ref={a => (this._inputElement = a)}
+              placeholder="enter task"
             />
-            <button type="submit"> Add Task </button>
+            <button type="submit">add</button>
           </form>
         </div>
+        <TodoItems entries={this.state.items} delete={this.deleteItem} />
       </div>
     );
   }
 }
 
-export default ToDoList;
+export default TodoList;
